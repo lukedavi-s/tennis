e49bb32f13037f8519b6c78ea3089d66c2d14d11
Luke Davis <76918134+lukedavisAR@users.noreply.github.com>
2021-08-17 11:19:02 -0500

    Add files via upload
    Updated pages, Aug 2021

---
+++ tennis.js
@@ -0,0 +1,26 @@
+//delay javascript execution until after all html and css is rendered.
+document.addEventListener("DOMContentLoaded", (event) => {
+  //page starts with icons, instead of full menu
+  document.getElementById('nav').className = 'iconsonly';
+  //event listener for clicking on hamburger menu
+  document.getElementById('burger').addEventListener('click', function() {
+    let menu = document.getElementById('nav')
+      //swap full menu / icons only styles
+      menu.className = menu.className == 'iconsonly' ? 'fullmenu' : 'iconsonly';
+  })
+  //this runs every time the page loads, doesn't need to be called as a function
+  let filename = location.pathname.split('/').pop();
+
+    /*otherwise loop through the rest of the links and apply highlight css style to
+    the link whose id matches the current page filename (without the extension)*/
+    let nav = document.getElementById('nav');
+    let links = nav.getElementsByTagName('a');
+    for (i = 0; i < links.length; i++) {
+      if (links[i].getAttribute('href').indexOf(filename) > -1) {
+        links[i].className = 'currentpage';
+      }
+    }
+
+
+  //make sure all JavaScript code is within this block
+})//end of document.addEventListener block
